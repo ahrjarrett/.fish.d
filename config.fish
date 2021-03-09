fb9a5533b1149ab2bbf54eb943f736c36b144f94
Andrew Jarrett <ahrjarrett@gmail.com>
2021-03-09 07:00:30 -0600

    update fish path, use env.fish file to shadow PATH using $fish_user_paths"

--- config.fish
+++ config.fish
@@ -1,9 +1,12 @@
 set -gx DOTFILES $HOME/dotfiles
-set -gx FISH_PATH $HOME/fish
+set -gx FISH_PATH $HOME/.fish.d
 set -gx FISH_FILE $FISH_PATH/config.fish
 set -gx BASH_FILE $HOME/.bash_profile
 set -gx VIM_FILE $HOME/nvim/init.vim
 set -gx EDITOR nvim
+set -gx DOTENVENC_KEY Swear!23
+
+set -gx GOOGLE_APPLICATION_CREDENTIALS $HOME/.config/gcloud/application_default_credentials.json
 
 set -gx fish_escape_delay_ms 10
 
@@ -36,11 +39,6 @@ end
 
 #rvm default
 
-# meant for Linux! (only used with RVM anyway)
-#source ~/.profile
-
-#set -g fish_user_paths "/usr/local/opt/mysql@5.6/bin" $fish_user_paths
-
 #source ~/.iterm2_shell_integration.(basename $SHELL)
 
 ### GCLOUD SDK
@@ -52,3 +50,6 @@ end
 [ -f /Users/aj/ownlocal/two-step/node_modules/tabtab/.completions/sls.fish ]; and . /Users/aj/ownlocal/two-step/node_modules/tabtab/.completions/sls.fish
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/aj/google-cloud-sdk/path.fish.inc' ]; . '/Users/aj/google-cloud-sdk/path.fish.inc'; end
+
+# opam configuration
+source /Users/aj/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
