d3d2bae8935c642aebd649c8d07b83726df7c6ca
Andrew Jarrett <ahrjarrett@gmail.com>
2022-04-01 06:40:13 -0500

    update abbr for `glg` fish abbreviation

--- functions/set_abbr.fish
+++ functions/set_abbr.fish
@@ -17,7 +17,7 @@ abbr -a -U  gs        git status
 abbr -a -U  gb        git branch
 abbr -a -U  gba       git branch -a
 abbr -a -U  gco       git checkout
-abbr -a -U  gcm       git commit -m \"
+abbr -a -U  gcm       git commit -m \"\"
 abbr -a -U  gpo       git push origin
 abbr -a -U  ghash     clipboard_git_hash
 abbr -a -U  shutdown  sudo shutdown -h now
@@ -34,7 +34,7 @@ abbr -a -U  showit    "defaults write com.apple.finder AppleShowAllFiles -bool t
 abbr -a -U  hideit    "defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
 
 # once you've run the following command to create a git alias:
-# git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)(%an)%Creset' --abbrev-commit"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)(%an)%Creset' --abbrev-commit"
 abbr -a -U  glg       git lg
 
 
